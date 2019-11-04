18321d4e452d9188a15167e2765babe138fae162
sameer <syedjava28@gmail.com>
2019-11-04 12:43:53 -0500

    add uploading files

---
+++ src/main/java/com/hellosameer/uploadingfiles/Application.java
@@ -0,0 +1,15 @@
+package com.hellosameer.uploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws IOException {
+        new File(UploadingController.uploadingDir).mkdirs();
+        SpringApplication.run(Application.class, args);
+    }
+}
